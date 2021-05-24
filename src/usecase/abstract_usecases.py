04398a83434f5ce3061b0fc36fc1274790a4a32e
Masahiro Ishii <marufeuille@gmail.com>
2021-05-24 22:38:31 +0900

    create abstract usecase

---
+++ src/usecase/abstract_usecases.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+from typing import Optional
+from datetime import datetime
+
+@dataclass
+class ToDoItemCreateInputData:
+    title: str
+    description: Optional[str]
+    start_date: Optional[datetime]
+    due_date: Optional[datetime]
+
+class ToDoItemCreateUseCase(metaclass=ABCMeta):
+    @abstractmethod
+    def handle(self, input_data: ToDoItemCreateInputData):
+        pass
+
+@dataclass
+class CalculateDueDateInputData:
+    due: datetime
+    include: bool = True
+
+class CalculateDueDateUseCase(metaclass=ABCMeta):
+    @abstractmethod
+    def handle(self, input_data: CalculateDueDateInputData):
+        pass
